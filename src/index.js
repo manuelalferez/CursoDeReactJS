3de1750e3a7e88eab2e263731a9a2e0e4fe57146
Manuel Alferez <manuelalferezruiz@gmail.com>
2019-04-13 09:47:32 +0200

    2.2 render

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+/* const element = document.createElement('h1')
+element.textContent= 'Hello World, Manuel!'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('app')
+container.append(element) */
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import React from "react";
+import { render } from "react-dom";
+
+const element = <h1>Hola Mundo!</h1>;
+const container = document.getElementById("app");
+/* render(qué, dónde); */
+render(element, container);
+/* ReactDOM.render(element, container); */
