836274079571039d98b5000c25817ff73a283155
Manuel Alferez <manuelalferezruiz@gmail.com>
2019-04-13 16:12:20 +0200

    Cómo aplicar estilos

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,30 +1,31 @@
 import React, { Component } from "react";
 import logoHeader from "../images/badge-header.svg";
-import avatar from "../images/avatar.png";
+import avatar from "../images/avatar.jpg";
+import './styles/Badge.css'
+
 
 class Badge extends Component {
   render() {
     return (
-      <div>
-        <div className="header">
-          <img src={logoHeader} />
+      <div className="badge">
+        <div className="badge_header">
+          <img src={logoHeader}  alt='logo'/>
         </div>
 
-        <div className="content">
-          <img src={avatar} />
+        <div className="badge_section-name ">
+          <img className="badge_avatar" alt='avatar' src={avatar} />
           <h1>
             Manuel <br /> Alférez
           </h1>
         </div>
 
-        <div className="description">
+        <div className="badge_section-info">
           <p>Develepoer and GDG organizer</p>
           <a href="https://www.manuelalferez.com" target="_blank">
             manuelalferez.com
           </a>
         </div>
-
-        <div>#devfest-2019</div>
+        <div className="badge_footer">#devfest-2019</div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { render } from "react-dom";
 import Badge from "./components/Badge";
+import 'bootstrap/dist/css/bootstrap.css'
+import './global.css'
 
 // Selección del elemento del DOM dónde insertar
 const container = document.getElementById("app");
